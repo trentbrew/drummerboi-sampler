23c48952a90abaa3d75072637012c38bcb011ee4
Trent Brew <tbrew1023@gmail.com>
2019-04-21 21:35:50 -0500

    added dancing:)

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,18 @@
 window.addEventListener("load", () => {
   const sounds = document.querySelectorAll(".sound");
   const pads = document.querySelectorAll(".pads div");
+  const image = document.getElementById("visual");
+  var rand = 0;
+  var currentImg = 0;
+
+  var img = [
+    "url('res/pose1.png')",
+    "url('res/pose2.png')",
+    "url('res/pose3.png')",
+    "url('res/pose4.png')",
+    "url('res/pose5.png')",
+    "url('res/pose6.png')"
+  ];
 
   console.log(sounds);
 
@@ -9,6 +21,15 @@ window.addEventListener("load", () => {
     pad.addEventListener('click', function() {
       sounds[index].currentTime = 0;
       sounds[index].play();
+      rand = Math.floor(Math.random() * 7);
+      console.log("currentImg: " + currentImg);
+      console.log("rand: " + rand);
+      console.log("");
+      while(currentImg == rand) {
+        rand = Math.floor(Math.random() * 7);
+      }
+      currentImg = rand;
+      image.style.backgroundImage = img[rand];
     });
   });
 });
